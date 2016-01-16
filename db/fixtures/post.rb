4ca5a7aabd6ec7a059c685202b5fd35d3be96f9c
y-temp4 <y.temp4@gmail.com>
2016-01-17 01:22:11 +0900

    seedファイルをそれっぽく修正

--- db/fixtures/post.rb
+++ db/fixtures/post.rb
@@ -1,8 +1,14 @@
 Post.seed(:id,
-  { id: 1, title: 'hgoehoge', body: 'fugafuga', user_id: 1, main_category_id: 1,
-    sub_category_id: 1 },
-  { id: 2, title: 'hgoehoge', body: 'fugafuga', user_id: 1, main_category_id: 1,
-    sub_category_id: 1 },
-  { id: 3, title: 'hgoehoge', body: 'fugafuga', user_id: 1, main_category_id: 1,
-    sub_category_id: 1 }
+  { id: 1, title: '床のきちんとした手入れ方法',
+    body: 'めちゃくちゃ拭きまくってください＞＜',
+    user_id: 1, main_category_id: 1, sub_category_id: 1 },
+  { id: 2, title: '床をふこう',
+    body: 'ふけふけふけ〜〜〜',
+    user_id: 1, main_category_id: 1, sub_category_id: 1 },
+  { id: 3, title: 'なんか記事のタイトル',
+    body: 'ここが本文です。',
+    user_id: 2, main_category_id: 1, sub_category_id: 1 },
+  { id: 4, title: 'ああああああああああああ',
+    body: 'いいいいいいいいいいいいいいいいいいいい',
+    user_id: 2, main_category_id: 1, sub_category_id: 1 }
 )
