1a38d4ab59081142f6edd4c3978c62c08dcb3cc5
y-temp4 <y.temp4@gmail.com>
2016-01-17 09:00:45 +0900

    デザインの修正

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -5,5 +5,7 @@ class SubCategoriesController < ApplicationController
   def show
     @sub_categories = SubCategory.where(main_category_id: params[:id])
     @main_categories = MainCategory.all
+
+    add_breadcrumb @main_categories.find_by(id: params[:id]).name, :root_path
   end
 end
